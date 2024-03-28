fecee712252185f4efe883c14bbc45770173215b
omar92 <ooomar92@gmail.com>
2024-03-28 16:51:53 +0200

    start default state on enable

--- StateMachineSM.cs
+++ StateMachineSM.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using SO;
 using UnityEngine.Events;
 
 namespace SO.SMachine
@@ -10,11 +7,11 @@ namespace SO.SMachine
     //[CreateAssetMenu(fileName = "GameState", menuName = "SM/Machine")]
     public class StateMachineSM : MonoBehaviour
     {
-        [SerializeField] GameStateSMSO CurrentGameState;
+        [SerializeField] private GameStateSMSO CurrentGameState;
         //[SerializeField] BoolSO IsGamePused;
-        [SerializeField] GameStateSM startState;
-        [SerializeField] UnityEvent onSwitchState;
-        
+        [SerializeField] private GameStateSM startState;
+        [SerializeField] private UnityEvent onSwitchState;
+
         private GameStateSM RuningGameState = null;
         //private bool _IsGamePaused;
         private void Awake()
@@ -22,11 +19,9 @@ namespace SO.SMachine
             CurrentGameState.Value = null;
             RuningGameState = null;
             CurrentGameState.Subscripe(OnStateChange);
-            //IsGamePused.Subscripe(OnGamePauseChange);
-            //_IsGamePaused = IsGamePused.Value;
         }
 
-        private void Start()
+        private void OnEnable()
         {
             if (startState != null)
                 CurrentGameState.Value = startState;
@@ -35,7 +30,6 @@ namespace SO.SMachine
         private void OnDestroy()
         {
             CurrentGameState.UnSubscripe(OnStateChange);
-            //IsGamePused.UnSubscripe(OnGamePauseChange);
         }
 
         private void OnStateChange(object sender, EventArgs e)
@@ -47,24 +41,9 @@ namespace SO.SMachine
                 Z.InvokeEndOfFrame(() =>
                 {
                     RuningGameState = CurrentGameState.Value;
-                    if (RuningGameState != null) { onSwitchState.Invoke();  RuningGameState.OnEnter();} else Debuger.LogWarning("Open Null state");
+                    if (RuningGameState != null) { onSwitchState.Invoke(); RuningGameState.OnEnter(); } else Debuger.LogWarning("Open Null state");
                 });
             });
         }
-        //private void OnGamePauseChange(object sender, EventArgs e)
-        //{
-        //    if (_IsGamePaused != IsGamePused.Value)//check that Value is inverted
-        //    {
-        //        _IsGamePaused = IsGamePused.Value;
-        //        if (_IsGamePaused)
-        //        {
-        //            RuningGameState.OnPause();
-        //        }
-        //        else
-        //        {
-        //            RuningGameState.OnUnPause();
-        //        }
-        //    }
-        //}
     }
 }
